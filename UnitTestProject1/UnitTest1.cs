e9a90c227a8f072f45df11f3802b596e24ea0a95
Sahil Gupta <Sahil.Gupta@bd.com>
2020-02-11 17:32:25 +0530

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("BD");
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("BD IS GOOD");
+
+        }
+    }
+}
